3f61388989d2b4db0417ad2d05229227943fdfd6
elmariofredo <mario@vejlupek.cz>
2017-10-28 00:51:44 +0200

    feat(Soos): print name argument

--- lib/soos.ex
+++ lib/soos.ex
@@ -15,4 +15,24 @@ defmodule Soos do
   def hello do
     :world
   end
+
+  def main(args) do
+    args |> parse_args |> process
+  end
+
+  def process([]) do
+    IO.puts "No arguments given"
+  end
+
+  def process(options) do
+    IO.puts "Hello #{options[:name]}"
+  end
+
+  defp parse_args(args) do
+    {options, _, _} = OptionParser.parse(args,
+      switches: [name: :string]
+    )
+    options
+  end
+
 end
