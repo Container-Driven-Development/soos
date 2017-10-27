ef70055515bd2f97290f8b2e6a47c3639e3012d6
elmariofredo <mario@vejlupek.cz>
2017-10-28 00:55:30 +0200

    feat(Project): add credo static code analysis

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule Soos.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 0.3", only: [:dev, :test]}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
